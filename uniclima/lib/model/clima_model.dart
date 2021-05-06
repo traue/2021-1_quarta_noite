b52cf3b4f1c3fc5132e22c9c7554232b7420fff8
Thiago G. Traue <t.traue@gmail.com>
2021-05-05 21:57:22 -0300

    Update do projeto

---
+++ uniclima/lib/model/clima_model.dart
@@ -0,0 +1,18 @@
+class ClimaData {
+  final double temp;
+  final double tempMax;
+  final double tempMin;
+  final String descTemp;
+  final String icone;
+  final int umidade;
+
+  ClimaData(
+      {this.temp,
+      this.tempMax,
+      this.tempMin,
+      this.descTemp,
+      this.icone,
+      this.umidade});
+
+  //Paramos aqui, falta criar a factory dela
+}
--- uniclima/lib/screens/home.dart
+++ uniclima/lib/screens/home.dart
@@ -8,6 +8,37 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+
+    List<String> _cidades = [
+      'Aracaju',
+      'Belém',
+      'Belo Horizonte',
+      'Boa Vista',
+      'Brasilia',
+      'Campo Grande',
+      'Cuiaba',
+      'Curitiba',
+      'Florianópolis',
+      'Fortaleza',
+      'Goiânia',
+      'João Pessoa',
+      'Macapá',
+      'Maceió',
+      'Manaus',
+      'Natal',
+      'Palmas',
+      'Porto Alegre',
+      'Porto Velho',
+      'Recife',
+      'Rio Branco',
+      'Rio de Janeiro',
+      'Salvador',
+      'São Luiz',
+      'São Paulo',
+      'Teresina',
+      'Vitoria'
+    ];
+
     return Container();
   }
 }
